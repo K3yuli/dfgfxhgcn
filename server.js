f0d280bc12cc9bc8de5c2938254c7b65873b2f1c
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-01-12 15:05:27 -0500

    post code

--- server.js
+++ server.js
@@ -74,6 +74,13 @@ app.get('api/animals/:id', (req, res) => {
     }
 });
 
+// route that listens for POST requests
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });       
