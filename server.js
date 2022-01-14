9f048e1446f0e0dfb308574ccc10f3dd3161f910
Kenia Lizama-Gamez <ke.yulizama@gmail.com>
2022-01-14 09:28:30 -0500

    update

--- server.js
+++ server.js
@@ -1,27 +1,18 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRouters');
-
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-// route that the front-end can request data from
-const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true}));
-// parse incoming JSON data
-app.use(express.json());
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-// middleware that instructs the server to make certain files readily available and to not gate it behind a server endpoint.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
+// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});       
+  console.log(`API server now on port ${PORT}!`);
+});   
